a394e5a0399f587d93cb9252212bae746a3e526c
singh44 <singh.vivek44@gmail.com>
2020-02-21 12:14:49 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular8-1991
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular8-1991)
---
+++ src/app/service/api.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {User} from "../model/user.model";
+import {Observable} from "rxjs/index";
+import {ApiResponse} from "../model/api.response";
+
+@Injectable()
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+  baseUrl: string = 'https://angular8-1991.stackblitz.io/users/';
+
+  login(loginPayload) : Observable<ApiResponse> {
+    return this.http.post<ApiResponse>('https://angular8-1991.stackblitz.io/' + 'token/generate-token', loginPayload);
+  }
+
+  getUsers() : Observable<ApiResponse> {
+    return this.http.get<ApiResponse>(this.baseUrl);
+  }
+
+  getUserById(id: number): Observable<ApiResponse> {
+    return this.http.get<ApiResponse>(this.baseUrl + id);
+  }
+
+  createUser(user: User): Observable<ApiResponse> {
+    return this.http.post<ApiResponse>(this.baseUrl, user);
+  }
+
+  updateUser(user: User): Observable<ApiResponse> {
+    return this.http.put<ApiResponse>(this.baseUrl + user.id, user);
+  }
+
+  deleteUser(id: number): Observable<ApiResponse> {
+    return this.http.delete<ApiResponse>(this.baseUrl + id);
+  }
+}
